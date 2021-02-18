69681c46ce8403ad9d988bedecef17edfc12ff36
Joshua Garde <joshgarde@gmail.com>
2021-02-18 00:26:03 -0800

    Initial auth endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,38 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors')
+const { MongoClient } = require('mongodb');
+const discord = require('./lib/discord');
 
 const app = express();
-
-app.get('/discord-auth', function (req, res) {
-  console.log(req.body);
-  res.end();
+const dbClient = new MongoClient(process.env.DATABASE_URL, {
+  useUnifiedTopology: true
 });
 
-app.listen(process.env.PORT);
+app.use(express.json());
+app.use(cors());
+
+app.post('/auth', async function auth(req, res) {
+  if (req.body && typeof req.body.code === 'string') {
+    let {code, redirectUri} = req.body;
+
+    try {
+      let discordAuth = await discord.authenticate(code, redirectUri);
+      res.send({success: true, message: ''});
+    } catch (err) {
+      console.log(err);
+      res.send({success: false, message: 'Discord request failed'})
+    }
+  } else {
+    res.send({success: false, message: 'Code is invalid/missing'})
+  }
+});
+
+dbClient.connect().then(() => {
+  let port = process.env.PORT;
+  app.listen(port);
+  console.log(`Now listening on :${port}`);
+}).catch((err) => {
+  console.log(`Unable to connect to db: ${err}`);
+});
---
+++ backend/lib/discord.js
@@ -0,0 +1,36 @@
+'use strict';
+const axios = require('axios');
+const qs = require('qs');
+
+const DISCORD_ENDPOINT = 'https://discord.com/api/v8';
+const DISCORD_ID = process.env.DISCORD_ID;
+const DISCORD_SECRET = process.env.DISCORD_SECRET;
+
+const instance = axios.create({
+  baseURL: DISCORD_ENDPOINT
+});
+
+module.exports = {
+  async authenticate(code, redirectUri) {
+    let requestData = qs.stringify({
+      client_id: DISCORD_ID,
+      client_secret: DISCORD_SECRET,
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: redirectUri,
+      scope: 'identify guilds'
+    });
+
+    let response = await axios.post(`${DISCORD_ENDPOINT}/oauth2/token`, requestData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      validateStatus: () => { return true }
+    });
+
+    console.log(redirectUri);
+    console.log(JSON.stringify(response.data));
+
+    return response.data;
+  }
+}
